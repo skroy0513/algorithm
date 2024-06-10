45dea7a2aae8fa32400e63389d93b2d16a03159e
skroy0513 <skroy0513@gmail.com>
2024-06-10 23:39:03 +0900

    solution1

---
+++ LinkedList/141. Linked List Cycle.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+
+            slow = slow.next
+            fast = fast.next.next
+        return True
